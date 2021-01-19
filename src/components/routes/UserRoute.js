5704c9f8d28e2670a2a41d1e07bbf4243ecf890a
leerivera <leerivera80@icloud.com>
2021-01-19 15:37:41 -0500

    test

--- src/components/routes/UserRoute.js
+++ src/components/routes/UserRoute.js
@@ -4,13 +4,9 @@ import { useSelector } from "react-redux";
 import LoadingToRedirect from "./LoadingToRedirect";
 
 const UserRoute = ({ children, ...rest }) => {
-    const { user } = useSelector((state) => ({ ...state }));
+  const { user } = useSelector((state) => ({ ...state }));
 
-    return user && user.token ? (
-        <Route {...rest} render={() => children} />
-    ) : (
-        <LoadingToRedirect />
-    );
+  return user && user.token ? <Route {...rest} /> : <LoadingToRedirect />;
 };
 
 export default UserRoute;
